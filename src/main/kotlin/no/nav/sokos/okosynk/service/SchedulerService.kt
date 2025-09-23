ebc1fde5be877c0e51d8791bee68bf9647bb82f4
takwaiwang <tak.wai.wang@nav.no>
2025-09-23 15:59:46 +0200

    fiks scheduler cancel job error

--- src/main/kotlin/no/nav/sokos/okosynk/service/SchedulerService.kt
+++ src/main/kotlin/no/nav/sokos/okosynk/service/SchedulerService.kt
@@ -10,7 +10,7 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
@@ -28,9 +28,8 @@ class SchedulerService(
     private val batchService: BatchService = BatchService(),
     private val jobTimeout: Duration = 30.minutes,
 ) {
-    private lateinit var schedulerJob: Job
-
-    private val scope = CoroutineScope(Dispatchers.Default)
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
     private val parser = CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.SPRING))
     private val isJobRunning = AtomicBoolean(false)
 
@@ -38,46 +37,47 @@ class SchedulerService(
         val cronDefinition = parser.parse(cronExpression)
         val executionTime = ExecutionTime.forCron(cronDefinition)
 
-        schedulerJob =
-            scope.launch {
-                while (isActive) {
-                    try {
-                        val now = ZonedDateTime.now()
-                        val nextExecution = executionTime.nextExecution(now).orElse(now)
-                        val delayMillis = ChronoUnit.MILLIS.between(now, nextExecution)
+        scope.launch {
+            while (isActive) {
+                try {
+                    val now = ZonedDateTime.now()
+                    val nextExecution = executionTime.nextExecution(now).orElse(now)
+                    val delayMillis = ChronoUnit.MILLIS.between(now, nextExecution)
 
-                        logger.info {
-                            val hours = delayMillis / 3_600_000
-                            val minutes = (delayMillis / 60_000) % 60
-                            val seconds = (delayMillis / 1000) % 60
-                            "Next execution scheduled at: $nextExecution (in ${hours}h ${minutes}m ${seconds}s)"
-                        }
-                        delay(delayMillis.coerceAtLeast(0))
-
-                        if (isJobRunning.compareAndSet(false, true)) {
-                            logger.debug { "Starting batch job execution" }
-                            withTimeoutOrNull(jobTimeout.absoluteValue) {
-                                batchService.run()
-                            } ?: throw TimeoutException("Job execution exceeded timeout of $jobTimeout")
-                        } else {
-                            logger.warn { "Skipping scheduled execution because previous job is still running" }
-                        }
-                    } catch (e: CancellationException) {
-                        logger.error(e) { "Batch job cancelled" }
-                        break
-                    } catch (e: TimeoutException) {
-                        logger.error(e) { "Batch job timed out after $jobTimeout" }
-                    } catch (e: Exception) {
-                        logger.error(e) { "Error executing batch job" }
-                        delay(1000)
+                    logger.info {
+                        val hours = delayMillis / (1000 * 60 * 60)
+                        val minutes = (delayMillis / (1000 * 60)) % 60
+                        val seconds = (delayMillis / 1000) % 60
+                        "Next execution scheduled at: $nextExecution (in ${hours}h ${minutes}m ${seconds}s)"
                     }
+                    delay(delayMillis.coerceAtLeast(0))
+
+                    if (isJobRunning.compareAndSet(false, true)) {
+                        logger.debug { "Starting batch job execution" }
+                        withTimeoutOrNull(jobTimeout.absoluteValue) {
+                            batchService.run()
+                        } ?: throw TimeoutException("Job execution exceeded timeout of $jobTimeout")
+                    } else {
+                        logger.warn { "Skipping scheduled execution because previous job is still running" }
+                    }
+                } catch (e: CancellationException) {
+                    logger.info { "Batch job cancelled" }
+                    break
+                } catch (e: TimeoutException) {
+                    logger.error(e) { "Batch job timed out after $jobTimeout" }
+                } catch (e: Exception) {
+                    logger.error(e) { "Error executing batch job" }
+                    delay(1000)
+                } finally {
+                    isJobRunning.set(false)
                 }
             }
+        }
         logger.info { "Batch job scheduler started with cron expression: $cronExpression" }
     }
 
     fun stop() {
-        schedulerJob.cancel()
+        job.cancel()
         logger.info { "Batch job scheduler stopped" }
     }
 }
