0678ac1e1e35d9750df9b88470e3cfabf85c857a
takwaiwang <tak.wai.wang@nav.no>
2025-09-29 13:33:15 +0200

    TOB-5421 nye faggrupper for oppdragssystemet

--- src/test/kotlin/no/nav/sokos/okosynk/config/RegelverkConfigTest.kt
+++ src/test/kotlin/no/nav/sokos/okosynk/config/RegelverkConfigTest.kt
@@ -3,21 +3,23 @@ package no.nav.sokos.okosynk.config
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.maps.shouldNotBeEmpty
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldMatch
 
 class RegelverkConfigTest :
     FunSpec({
+        val keyPattern = Regex("^[A-Za-z0-9]+_[0-9]+$")
+
         test("Les inn mapping regel for OS") {
             val regelverkMap = RegelverkConfig.regelverkOSMap
             regelverkMap.shouldNotBeEmpty()
-            regelverkMap.size shouldBe 84
-            regelverkMap.forEach { (key, _) -> key shouldContain "_" }
+            regelverkMap.size shouldBe 86
+            regelverkMap.forEach { (key, _) -> key shouldMatch keyPattern }
         }
 
         test("Les inn mapping regel for UR") {
             val regelverkMap = RegelverkConfig.regelverkURMap
             regelverkMap.shouldNotBeEmpty()
             regelverkMap.size shouldBe 70
-            regelverkMap.forEach { (key, _) -> key shouldContain "_" }
+            regelverkMap.forEach { (key, _) -> key shouldMatch keyPattern }
         }
     })
